75fe5c8e1c7640dbca904e8b89cbb8d406bf7c13
Sun <huachuang20@gmail.com>
2019-03-14 15:18:53 +0800

    add litter prime rsa

--- lib/rsa.ex
+++ lib/rsa.ex
@@ -1,18 +1,106 @@
-defmodule Rsa do
+defmodule RSA do
   @moduledoc """
-  Documentation for Rsa.
+  Documentation for RSA.
   """
 
-  @doc """
-  Hello world.
+  defp big_num(s) do
+    Enum.reduce(1..s+1, fn _, y -> y * 2 end)
+  end
 
-  ## Examples
+  defp big_pow_mod(x, s, n) do
+    Enum.reduce(1..s+1, fn _, y -> y * x end) |> rem(n)
+  end
 
-      iex> Rsa.hello()
-      :world
+  defp miller_rabin(n, k, r, s) do
+    try do
+      for _ <- 0..k do
+        a = Enum.random(2..n-1)
+        x = big_pow_mod(a, s, n)
+        if x != 1 && x != n - 1 do
+          for _ <- 0..r-1 do
+            x = big_pow_mod(x, 2, n)
+            if x == n - 1, do: throw(:true)
+          end
+          throw(:false)
+        end
+      end
 
-  """
-  def hello do
-    :world
+      true
+    catch
+      :false -> false
+      :true -> true
+    end
+  end
+
+  defp max_cardinal(r, n) when rem(n, 2) != 0, do: {r, n}
+
+  defp max_cardinal(r, n) do
+    max_cardinal(r+1, div(n, 2))
+  end
+
+  defp is_prime?(n) when n in [2, 3], do: true
+
+  defp is_prime?(n) when rem(n, 2) == 0, do: false
+
+  defp is_prime?(n) do
+    {r, s}= max_cardinal(0, n - 1)
+    miller_rabin(n, 100, r, s)
+  end
+
+  defp prng() do
+    :crypto.rand_uniform(big_num(5), big_num(6))
+  end
+
+  defp prng_prime() do
+    n = prng()
+    if is_prime?(n) == true, do: n, else: prng_prime()
+  end
+
+  defp gcd(a, b) when b == 0, do: a
+
+  defp gcd(a, b) do
+    gcd(b, rem(a, b))
+  end
+
+  defp lcm(a, b) do
+    a * b / gcd(a, b) |> trunc
+  end
+
+  def rng_e(l) do
+    Enum.find 2..l, &(gcd(&1, l) == 1)
+  end
+
+  def rng_d(e, l) do
+    Enum.find 2..l, &rem(e * &1, l) == 1
+  end
+
+  def generate() do
+    q = prng_prime()
+    p = prng_prime()
+    n = p * q
+    l = lcm(q - 1, p - 1)
+    e = rng_e(l)
+    d = rng_d(e, l)
+    {{e, n}, {d, n}}
+  end
+
+  def encrypt(plaintext, {e, n}) do
+    p = plaintext
+    big_pow_mod(p, e, n)
+  end
+
+  def decrypt(ciphertext, {d, n}) do
+    c = ciphertext
+    big_pow_mod(c, d, n)
+  end
+
+  def sign(plaintext, {d, n}) do
+    p = plaintext
+    big_pow_mod(p, d, n)
+  end
+
+  def verify(plaintext, signature, {e, n}) do
+    s = signature
+    big_pow_mod(s, e, n) == plaintext
   end
 end
--- test/rsa_test.exs
+++ test/rsa_test.exs
@@ -1,8 +1,20 @@
 defmodule RsaTest do
   use ExUnit.Case
-  doctest Rsa
+  doctest RSA
 
-  test "greets the world" do
-    assert Rsa.hello() == :world
+  test "test generate, encrypt, decrypt" do
+    {{pk, n}, {psk, n}} = RSA.generate()
+    IO.puts "PK is #{pk}, PSK is #{psk}, n is #{n}"
+    plaintext = 10
+    ciphertext = RSA.encrypt(plaintext, {pk, n})
+    assert RSA.decrypt(ciphertext, {psk, n}) == plaintext
+  end
+
+  test "test sign, verify" do
+    {{pk, n}, {psk, n}} = RSA.generate()
+    IO.puts "PK is #{pk}, PSK is #{psk}, n is #{n}"
+    plaintext = 10
+    signature = RSA.sign(plaintext, {pk, n})
+    assert RSA.verify(plaintext, signature, {psk, n}) == true
   end
 end
